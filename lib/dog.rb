dfc8f49d1fb04264b75ac6e0eba094f3e0b1129d
Patrick Wu <Ptrckwu@yahoo.com>
2021-10-17 02:03:49 -0400

    commit tree 88ac2a27909595a82f42cfc685843242768c9779

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,28 @@
-class Dog
+require "active_record"
+require "pry"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/dogs.sqlite"
+  )
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+)
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
+    # def initialize(attributes) 
+    #     attributes.each do |key, value| 
+    #         if self.respond_to?("#{key.to_s}=")
+    #             self.send("#{key.to_s}=", value) 
+    #         end 
+    #     end
+    # end
 end
+
+binding.pry
